077396b5db22dc07928425e6fb521ce5c81611cd
JINKOO <jacob777300@gmail.com>
2022-02-15 23:02:08 +0900

    8장 Fragment추가

--- app/src/main/java/com/kjk/criminalintent/view/MainActivity.kt
+++ app/src/main/java/com/kjk/criminalintent/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kjk.criminalintent.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.kjk.criminalintent.R
 import com.kjk.criminalintent.databinding.ActivityMainBinding
 
@@ -14,5 +15,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            Log.d(TAG, "onCreate: ")
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
