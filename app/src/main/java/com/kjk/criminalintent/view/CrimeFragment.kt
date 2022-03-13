b24710ff8b23886b5e88ac2d22b5447acf78beb6
JINKOO <jacob777300@gmail.com>
2022-03-13 16:44:27 +0900

    15장 암시적 인텐트(챌린지 해야함)

--- app/src/main/java/com/kjk/criminalintent/view/CrimeFragment.kt
+++ app/src/main/java/com/kjk/criminalintent/view/CrimeFragment.kt
@@ -1,11 +1,18 @@
 package com.kjk.criminalintent.view
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +20,7 @@ import com.kjk.criminalintent.R
 import com.kjk.criminalintent.data.Crime
 import com.kjk.criminalintent.data.CrimeDetailViewModel
 import com.kjk.criminalintent.databinding.FragmentCrimeBinding
+import com.kjk.criminalintent.extension.Date.FORTMAT_LONGLONG
 import java.util.*
 
 // Controller (Model과 View와 상호 작용한다.)
@@ -22,7 +30,6 @@ class CrimeFragment :
     DatePickerFragment.CallBacks {
 
     private var _binding: FragmentCrimeBinding? = null
-
     private val binding
         get() = _binding!!
 
@@ -32,6 +39,8 @@ class CrimeFragment :
 
     private lateinit var crime: Crime
 
+    private lateinit var getResultSuspect: ActivityResultLauncher<Intent>
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         Log.d(TAG, "onAttach(): ")
@@ -40,14 +49,10 @@ class CrimeFragment :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(): ")
-
         setHasOptionsMenu(true)
-
         crime = Crime()
-
         val crimeID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
         Log.d(TAG, "onCreate: ${crimeID}")
-
         // 궁극적으로는 database로 부터 data를 로드해야 한다.
         crimeDetailViewModel.loadCrime(crimeID)
     }
@@ -59,6 +64,36 @@ class CrimeFragment :
     ): View {
         Log.d(TAG, "onCreateView(): ")
         _binding = FragmentCrimeBinding.inflate(inflater, container, false)
+
+        getResultSuspect = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val contactUri = result.data?.data ?: return@registerForActivityResult
+
+                // 쿼리에서 값으로 반환할 필드를 지정한다.
+                val queryFields = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+
+                // 쿼리를 수행한다. contactUri는 콘텐츠 제공자의 테이블을 나타낸다.
+                val cursor = requireActivity().contentResolver
+                    .query(contactUri, queryFields, null, null, null)
+
+                cursor?.use {
+                    // 쿼리 결과 데이터가 있는지 확인한다.
+                    if (it.count == 0) {
+                        return@registerForActivityResult
+                    }
+
+                    // 첫번째 데이터 행의 첫번째 속상 값을 가져 온다.
+                    // 이 값이 용의자의 이름이다.
+                    it.moveToFirst()
+                    val suspect = it.getString(0)
+                    crime.suspect = suspect
+                    crimeDetailViewModel.saveCrime(crime)
+                    binding.selectSuspectButton.text = suspect
+                }
+            }
+        }
+
         return binding.root
     }
 
@@ -80,6 +115,8 @@ class CrimeFragment :
             crime.date = result
             updateUI()
         }
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -129,6 +166,35 @@ class CrimeFragment :
                     show(this@CrimeFragment.parentFragmentManager, DIALOG_TIME)
                 }
             }
+
+            // 범죄 보고서 전송을 위한 이벤트
+            sendCriminalReportButton.setOnClickListener {
+                Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(
+                        Intent.EXTRA_TEXT,  // 메세지의 데이터
+                        getCrimeReport())
+                    putExtra(
+                        Intent.EXTRA_SUBJECT, // 메세지의 제목
+                        getString(R.string.crime_report_subject)
+                    )
+                }.also { intent ->  // also 사용하는 경우 : 객체의 속성을 전혀 사용하지 않거나, 변경하지 않는 경우에 사용한다.
+                    //startActivity(intent)
+                    // 사용자에게 어떤 액티비티를 사용할 것인지, 선택권을 줄 수 있도록 한다.
+                    // 암시적 인텐트에 응답하는 액티비티들을 보여줄 선택기를 생성하는 코드
+                    val chooserIntent = Intent.createChooser(intent, getString(R.string.send_report))
+                    startActivity(chooserIntent)
+                }
+            }
+
+            // 연락처에서 용의자 선택
+            selectSuspectButton.apply {
+                val pickContactIntent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+
+                setOnClickListener {
+                    getResultSuspect.launch(pickContactIntent)
+                }
+            }
         }
     }
 
@@ -142,6 +208,26 @@ class CrimeFragment :
                 jumpDrawablesToCurrentState()
             }
         }
+    }
+
+    // 15장 범죄 보고서 전송(암시적 Intent)
+    private fun getCrimeReport(): String {
+        val solvedString = if (crime.isSolved) {
+            getString(R.string.crime_report_solved)
+        } else {
+            getString(R.string.crime_report_unsolved)
+        }
+
+        val dateString = DateFormat.format(FORTMAT_LONGLONG, crime.date).toString()
+
+        val suspect = if (crime.suspect.isBlank()) {
+            getString(R.string.crime_report_no_suspect)
+        } else {
+            getString(R.string.crime_report_suspect, crime.suspect)
+        }
+
+        return getString(R.string.crime_report,
+            crime.title, dateString, solvedString, suspect)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -207,6 +293,7 @@ class CrimeFragment :
         private const val DIALOG_TIME = "DialogTime"
         const val REQUEST_DATE = "request_date"
         const val REQUEST_TIME = "request_time"
+        const val REQUEST_SUSPECT = "request_suspect"
         fun newInstance(crimeID: UUID): CrimeFragment {
             val args = Bundle().apply {
                 putSerializable(ARG_CRIME_ID, crimeID)
